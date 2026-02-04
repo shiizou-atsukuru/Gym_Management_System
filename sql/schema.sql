b660c22067d20a6e37f11f02a74723563124ad7f
Shiizou Atsukuru <slicedheaven314@gmail.com>
2026-02-04 22:18:12 +0530

    Added serialization to IDs. Added subscription details view

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,8 @@
 CREATE TABLE members (
-    member_id CHAR(50),
+    internal_id SERIAL,
+    member_id CHAR(50) GENERATED ALWAYS AS (
+    'M' || LPAD(internal_id::text, 49, '0')
+    ) STORED, 
     member_name VARCHAR(200) NOT NULL,
     member_phoneno BIGINT NOT NULL,
     member_address VARCHAR(500) NOT NULL,
@@ -8,7 +11,10 @@ CREATE TABLE members (
 );
 
 CREATE TABLE staff (
-    staff_id CHAR(50),
+    internal_id SERIAL,
+    staff_id CHAR(50) GENERATED ALWAYS AS (
+    'E' || LPAD(internal_id::text, 49, '0')
+    ) STORED, 
     staff_name VARCHAR(200) NOT NULL,
     staff_role VARCHAR(50) NOT NULL,
     PRIMARY KEY(staff_id),
@@ -16,7 +22,10 @@ CREATE TABLE staff (
 );
 
 CREATE TABLE branches (
-    branch_id CHAR(50),
+    internal_id SERIAL,
+    branch_id CHAR(50) GENERATED ALWAYS AS (
+    'B' || LPAD(internal_id::text, 49, '0')
+    ) STORED, 
     branch_address VARCHAR(500) NOT NULL,
     branch_capacity INT NOT NULL,
     manager_id CHAR(50) NOT NULL,
@@ -34,7 +43,10 @@ CREATE TABLE works_at (
 );
 
 CREATE TABLE service_types (
-    service_type_id CHAR(50),
+    internal_id SERIAL,
+    service_type_id CHAR(50) GENERATED ALWAYS AS (
+    'SE' || LPAD(internal_id::text, 48, '0')
+    ) STORED, 
     service_name VARCHAR(50) NOT NULL UNIQUE,
     PRIMARY KEY(service_type_id)
 );
@@ -48,7 +60,10 @@ CREATE TABLE trainer_specialization (
 );
 
 CREATE TABLE class_sessions (
-    session_id CHAR(100),
+    internal_id SERIAL,
+    session_id CHAR(100) GENERATED ALWAYS AS (
+    'SS' || LPAD(internal_id::text, 98, '0')
+    ) STORED, 
     service_type_id CHAR(50) NOT NULL,
     trainer_id CHAR(50) NOT NULL,
     branch_id CHAR(50) NOT NULL,
@@ -58,7 +73,7 @@ CREATE TABLE class_sessions (
     FOREIGN KEY(service_type_id) REFERENCES service_types(service_type_id),
     FOREIGN KEY(trainer_id) REFERENCES staff(staff_id),
     FOREIGN KEY(branch_id) REFERENCES branches(branch_id),
-    CHECK(total_seats>0),
+    CHECK(total_seats>0)
 );
 
 CREATE TABLE booking (
@@ -78,7 +93,10 @@ CREATE TABLE offered_services (
 );
 
 CREATE TABLE membership_plans (
-    plan_id CHAR(5),
+    internal_id SERIAL,
+    plan_id CHAR(5) GENERATED ALWAYS AS (
+    'P' || LPAD(internal_id::text, 4, '0')
+    ) STORED, 
     tier_name VARCHAR(50) NOT NULL UNIQUE,
     fee INT NOT NULL,
     duration INT NOT NULL,
@@ -88,22 +106,27 @@ CREATE TABLE membership_plans (
 );
 
 CREATE TABLE subscriptions (
-    subscription_id char(100),
+    internal_id SERIAL,
+    subscription_id char(100) GENERATED ALWAYS AS (
+    'SU' || LPAD(internal_id::text, 98, '0')
+    ) STORED,
     member_id CHAR(50),
     plan_id CHAR(5),
     branch_id CHAR(50),
     payment_date DATE,
     start_date DATE,
-    expiry_date DATE NOT NULL,
-    status VARCHAR(10) NOT NULL,
     PRIMARY KEY(subscription_id),
     FOREIGN KEY(member_id) REFERENCES members(member_id),
     FOREIGN KEY(plan_id) REFERENCES membership_plans(plan_id),
-    FOREIGN KEY(branch_id) REFERENCES branches(branch_id)
+    FOREIGN KEY(branch_id) REFERENCES branches(branch_id),
+    UNIQUE(member_id, plan_id, branch_id, payment_date)
 );
 
 CREATE TABLE inventory_items (
-    item_id CHAR(50),
+    internal_id SERIAL,
+    item_id CHAR(50) GENERATED ALWAYS AS (
+    'I' || LPAD(internal_id::text, 49, '0')
+    ) STORED, 
     item_name VARCHAR(100) NOT NULL UNIQUE,
     PRIMARY KEY(item_id)
 );
@@ -118,11 +141,13 @@ CREATE TABLE branch_inventories (
     CHECK(quantity>0)
 );
 
+
 -- Views
 
-CREATE OR REPLACE VIEW view_session_availability AS
+CREATE OR REPLACE VIEW session_availability AS
 SELECT 
     cs.session_id,
+    cs.branch_id,
     st.service_name,
     s.staff_name AS trainer_name,
     cs.schedule_time,
@@ -138,5 +163,22 @@ JOIN
 LEFT JOIN 
     booking b ON cs.session_id = b.session_id
 GROUP BY 
-    cs.session_id, st.service_name, s.staff_name, cs.schedule_time, cs.total_seats;
+    cs.session_id, st.service_name, s.staff_name, cs.schedule_time, cs.branch_id, cs.total_seats;
 
+
+CREATE OR REPLACE VIEW subscription_details AS
+SELECT 
+    s.subscription_id,
+    s.member_id,
+    s.start_date,
+    -- Calculate expiry_date: start_date + plan duration (in months)
+    (s.start_date + (p.duration || ' months')::interval)::date AS computed_expiry_date,
+    -- Calculate status based on that new expiry date
+    CASE 
+        WHEN (s.start_date + (p.duration || ' months')::interval)::date >= CURRENT_DATE THEN 'ACTIVE'
+        ELSE 'EXPIRED'
+    END AS computed_status
+FROM 
+    subscriptions s
+JOIN 
+    membership_plans p ON s.plan_id = p.plan_id;
